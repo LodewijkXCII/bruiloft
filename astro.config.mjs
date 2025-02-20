8d06dda1a17b56c1c31af431d2b52ee95d33d4a1
LodewijkXCII <loek@zweersdesign.com>
2025-02-20 21:23:30 +0100

    ädding vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 import vue from "@astrojs/vue";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [db(), vue({ jsx: true })],
+  adapter: vercel(),
 });
